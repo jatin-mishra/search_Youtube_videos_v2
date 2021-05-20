ace98f18339b6cdecad913311aeee565ae402779
jatinMishra <jatinmishra2k99@gmail.com>
2021-05-20 12:28:37 +0530

    added default search options with no <pk> specified: searchingDefaultView at searchingApp.views.searchingDefaultView

--- searchingApp/urls.py
+++ searchingApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import searchingView
+from .views import searchingView, searchingDefaultView
 
 
 urlpatterns = [
     path('<int:n>', searchingView.as_view()),
+    path('', searchingDefaultView.as_view()),
 ]
 
--- searchingApp/views.py
+++ searchingApp/views.py
@@ -15,6 +15,36 @@ from django.http import HttpResponse
 import json
 import requests
 
+searchingDefaultView
+
+
+class searchingDefaultView(APIView):
+    def get(self, request):
+        n = 5
+        query = request.data.get('query','')
+
+        if query or len(YoutubeData.objects.all()) < n:
+            if not n:
+                n = 10
+                
+            if not query:
+                query = 'celery tutorial'
+
+            alldata = fetch_youtube_data(n,query, False)
+            all_objects = []
+            for videoid in alldata:
+                if YoutubeData.objects.filter(video_id=videoid).exists():
+                    video_instance = YoutubeData(video_id=videoid,published_date=alldata[videoid]['published_at'], title=alldata[videoid]['title'], description=alldata[videoid]['description'], actual_link=alldata[videoid]['url'])
+                    all_objects.append(video_instance)
+            serializer = YoutubeDataSerializer(all_objects, many=True)
+        else:
+            if not n:
+                n=10
+            all_records = YoutubeData.objects.all().order_by('-published_date')[:n] 
+            serializer = YoutubeDataSerializer(all_records, many=True)
+            
+
+        return Response(serializer.data)
 
 # Create your views here.
 
